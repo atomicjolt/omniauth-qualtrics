aef8d8682bbaee06751425dbfc456d942507546f
Stephen Mariano Cabrera <stephen@userinterviews.com>
2022-06-30 17:06:31 -0400

    WIP

--- lib/omniauth/strategies/qualtrics.rb
+++ lib/omniauth/strategies/qualtrics.rb
@@ -1,15 +1,14 @@
-require "omniauth-oauth2"
+require 'omniauth-oauth2'
 
 module OmniAuth
   module Strategies
     class Qualtrics < OmniAuth::Strategies::OAuth2
-
-      option :name, "qualtrics"
+      option :name, 'qualtrics'
 
       option :client_options,
-            site:          "https://co1.qualtrics.com",
-            authorize_url: "/oauth2/auth",
-            token_url:     "/oauth2/token"
+             site: 'https://co1.qualtrics.com',
+             authorize_url: '/oauth2/auth',
+             token_url: '/oauth2/token'
 
       # Qualtrics does use state but we want to control it rather than letting
       # omniauth-oauth2 handle it.
@@ -19,8 +18,12 @@ module OmniAuth
 
       info do
         {
-          "url" => access_token.client.site
+          'url' => access_token.client.site
         }
+      end
+
+      def callback_url
+        full_host + script_name + callback_path
       end
 
       # Override authorize_params so that we can be deliberate about the value for state
@@ -29,16 +32,17 @@ module OmniAuth
       def authorize_params
         # Only set state if it hasn't already been set
         options.authorize_params[:state] ||= SecureRandom.hex(24)
-        params = options.authorize_params.merge(options_for("authorize"))
+        options.authorize_params[:scope] = 'manage:all'
+        params = options.authorize_params.merge(options_for('authorize'))
         if OmniAuth.config.test_mode
           @env ||= {}
-          @env["rack.session"] ||= {}
+          @env['rack.session'] ||= {}
         end
         params
       end
-
     end
   end
 end
 
-OmniAuth.config.add_camelization "qualtrics", "Qualtrics"
+OmniAuth.config.add_camelization 'qualtrics', 'Qualtrics'
+
